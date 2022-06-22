19410ee83bef28ce6eb705378234924d2a6787da
Youssef Ragab <ragab.youssef95@gmail.com>
2022-06-22 10:47:53 -0400

    implement basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Typography, AppBar, Toolbar, IconButton } from '@mui/material';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+        <AppBar color='secondary'>
+          <Toolbar>
+            <Button variant='contained' size='large'>
+              <RestaurantMenuIcon />
+              Browse Menu
+            </Button>
+            {/* <IconButton>
+              <RestaurantMenuIcon />
+            </IconButton> */}
+            <Typography variant="h4">
+              Chill Grill Restaurant
+            </Typography>
+            <div id="user">
+              <Button variant='contained' size='large'>
+                Login
+              </Button>
+              <Button variant='contained' size='large'>
+                Sign Up
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
     </div>
   );
 }
