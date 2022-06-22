94ccb240c11ad9f567180621446abeb0ae770adf
Youssef Ragab <ragab.youssef95@gmail.com>
2022-06-22 12:09:26 -0400

    style navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,13 @@
 import './App.css';
-import {Button, Typography, AppBar, Toolbar, IconButton } from '@mui/material';
-import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
       </header>
-        <AppBar color='secondary'>
-          <Toolbar>
-            <Button variant='contained' size='large'>
-              <RestaurantMenuIcon />
-              Browse Menu
-            </Button>
-            {/* <IconButton>
-              <RestaurantMenuIcon />
-            </IconButton> */}
-            <Typography variant="h4">
-              Chill Grill Restaurant
-            </Typography>
-            <div id="user">
-              <Button variant='contained' size='large'>
-                Login
-              </Button>
-              <Button variant='contained' size='large'>
-                Sign Up
-              </Button>
-            </div>
-          </Toolbar>
-        </AppBar>
     </div>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,37 @@
-import React from "react";
+import {Button, Typography, AppBar, Toolbar, Stack } from '@mui/material';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import { makeStyles } from '@mui/styles';
 
-import Button from '@mui/material'
+// const useStyles = makeStyles({
+//   navbar: {
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//   }
+// })
+
+export default function Navbar(props) {
+  // const classes = useStyles()
+
+  return(
+    <AppBar color='secondary'>
+      <Toolbar style={{ width:'100%', display: 'inline-flex', justifyContent: 'space-between' }}>
+        <Button variant='contained' size='large'>
+          <RestaurantMenuIcon />
+          Browse Menu
+        </Button>
+        <Typography variant="h4">
+          Chill Grill Restaurant
+        </Typography>
+        <div>
+          <Button variant='contained' size='large'>
+            Login
+          </Button>
+          <Button variant='contained' size='large'>
+            Sign Up
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
+  )
+}
 
