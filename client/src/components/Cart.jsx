18a6feb4b2623b31fcbaa46ee7468c3480de4dfe
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-04 10:25:11 -0400

    initiate cart functionality

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -4,7 +4,15 @@ import axios from "axios";
 import { UserContext } from '../context/userContext';
 
 export default function Cart(props) {
-  
+
+  const { userContextCart, setCart } = useContext(UserContext);
+
+  // console.log(userContextCart)
+ 
+  for (let item in userContextCart) {
+    console.log(userContextCart[item])
+  }
+
   return(
     <>
       <h1>
