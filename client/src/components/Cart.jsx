691c156775a92c7f07e4b891389521671c9c2959
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-06 19:58:04 -0400

    style cart

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,10 +1,20 @@
 import React, { useContext } from "react";
 import { useNavigate } from 'react-router-dom';
-import {Button, Typography } from '@mui/material';
+import {Button, Typography, Box } from '@mui/material';
 import axios from "axios";
 import { UserContext } from '../context/userContext';
+import { makeStyles } from "@mui/material";
+
+const useStyles = makeStyles({
+  root: {
+    width: '80%',
+    marginLeft: '20px',
+  },
+})
 
 export default function Cart(props) {
+
+  const classes = useStyles;
 
   const { userContextCart, setCart, userContextMenuItems, setMenuItems, userContextOrderId } = useContext(UserContext);
 
@@ -61,15 +71,65 @@ export default function Cart(props) {
     if (item.quant > 0) {
 
       return(
-        <>
-          <h1>
+        <div className={classes.root}>
+          <Box 
+            sx={{ 
+              display: 'flex', 
+              flexDirection: 'row', 
+              justifyContent: 'space-between' 
+            }}
+          >
+            <Box 
+              sx={{ 
+                display: 'flex', 
+                flexDirection: 'row', 
+                justifyContent: 'space-between' 
+              }}
+            >
+              <Typography variant= "h2">
+                {item.quant} {item.name}
+              </Typography>
+              <Button 
+                sx={{
+                  borderRadius: 28,
+                  height: 40,
+                  width: 40,
+                  alignSelf: 'center',
+                  marginLeft: 2
+                }}
+                variant= 'outlined'
+                onClick={() => addToUserContextCart(item.id)}
+              >
+                +
+              </Button>
+              <Button 
+                sx={{
+                  borderRadius: 28,
+                  height: 40,
+                  width: 40,
+                  alignSelf: 'center',
+                  marginLeft: 2
+                }}
+                variant= 'outlined'
+                onClick={() => decreaseFromUserContextCart(item.id)}
+              >
+                -
+              </Button>
+            </Box>
+            <Box>
+              <Typography variant="h2">
+              {item.quant * item.price}$
+              </Typography>
+            </Box>
+          </Box>
+          {/* <h1>
             name: {item.name}<br />
             item id: {item.id}<br />
             quantity: {item.quant}<br />
             item price: {item.price}<br />
             price: {item.quant} X {item.price} = {item.quant * item.price}
-          </h1>
-          <Button
+          </h1> */}
+          {/* <Button
             variant='contained' 
             size='large'
             onClick={() => addToUserContextCart(item.id)}
@@ -89,8 +149,8 @@ export default function Cart(props) {
             onClick={() => clearItemFromCart(item.id)}
           >
             Clear Item
-          </Button>
-        </>
+          </Button> */}
+        </div>
       )
     }
   })
